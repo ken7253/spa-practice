1d3e3383fdf0caada15f533aa3889e6aac7bcc65
ken7253 <ken7253@gmail.com>
2022-07-30 15:01:22 +0900

    WIP:ローカル環境で都道府県一覧を表示できるように

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
-import React from "react";
-import LineGraph from "./components/LineGraph";
+import React, { useEffect, useState } from "react";
+import LineGraph from "./components/LineGraph/LineGraph";
+import CheckBox from "./components/CheckBox/CheckBox";
 import "./App.css";
+
+interface Prefectures {
+  message: null,
+  result: Result[]
+}
+
+interface Result {
+  prefCode: number,
+  prefName: string
+}
 
 const App: React.FC = () => {
 
+  const [prefectures, setPrefectures] = useState<Result[]>();
+
+  useEffect(() => {
+    const APIData = fetch('http://localhost:8888/.netlify/functions/get-prefectures');
+
+    void APIData.then(async (resp) => {
+      const json = await resp.json() as Record<keyof Prefectures, unknown>;
+      if (typeof json !== "object" || json === null) return;
+      if (Array.isArray(json.result)) {
+        setPrefectures(json.result);
+      }
+    })
+
+    return () => {
+      setPrefectures(undefined);
+    }
+  },[])
+
   return (
     <div className="App">
-      <LineGraph />
+      <LineGraph>
+        {prefectures?.map((value) => {
+          return(<CheckBox key={value.prefCode}>{value.prefName}</CheckBox>);
+        })}
+      </LineGraph>
     </div>
   );
 };
