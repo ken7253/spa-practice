3d5521a4452dd868f8a5aefe4e2ed144c802fe85
ken7253 <ken7253@gmail.com>
2022-07-31 23:31:34 +0900

    fmt

--- src/components/LineGraph/LineGraph.tsx
+++ src/components/LineGraph/LineGraph.tsx
@@ -1,29 +1,35 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import "./LineGraph.css";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
-const LineGraph: React.FC = () => {
+interface Props {
+  children?: ReactNode;
+  showDataId?: (number | string)[];
+}
+
+const LineGraph: React.FC<Props> = (props: Props) => {
   const options: Highcharts.Options = {
     title: {
-      text: 'テスト',
+      text: "テスト",
     },
     series: [
       {
         name: "test",
         type: "line",
-        data: [1, 2, 3]
+        data: [1, 2, 3],
       },
       {
         name: "test2",
         type: "line",
         data: [10, 5, 30],
-      }
+      },
     ],
   };
 
   return (
     <>
+      <div className="controller">{props.children}</div>
       <HighchartsReact highcharts={Highcharts} options={options} />
     </>
   );
