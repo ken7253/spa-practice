7260e1d7078c1b8680ba9817e603f7fb5ddce8a0
ken7253 <ken7253@gmail.com>
2022-08-01 23:09:31 +0900

    取得したデータをグラフに反映する処理を追加

--- src/components/LineGraph/LineGraph.tsx
+++ src/components/LineGraph/LineGraph.tsx
@@ -1,9 +1,22 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import "./LineGraph.css";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
 import type { Result } from "../../types/prefectures";
+
+interface Response {
+  label: string;
+  data: {
+    year: number;
+    value: number;
+  }[];
+}
+
+interface chartData {
+  name: string;
+  data: number[];
+}
 
 interface Props {
   children?: ReactNode;
@@ -13,23 +26,47 @@ interface Props {
 }
 
 const LineGraph: React.FC<Props> = (props: Props) => {
+  const [graph, setGraph] = useState<chartData[]>();
+
   const options: Highcharts.Options = {
     title: {
       text: props.title,
     },
-    series: [
-      {
-        name: "test",
+    series: graph?.map((population) => {
+      return {
+        name: population.name,
         type: "line",
-        data: [1, 2, 3],
-      },
-      {
-        name: "test2",
-        type: "line",
-        data: [10, 5, 30],
-      },
-    ],
+        data: population.data,
+      };
+    }),
   };
+
+  useEffect(() => {
+    if (!props.showDataId) return;
+    const requestURL =
+      "http://localhost:8888/.netlify/functions/get-population";
+    const APIData = props.showDataId?.map((value) => {
+      return fetch(`${requestURL}?prefCode=${value}`);
+    });
+
+    const converter = async (): Promise<chartData[]> => {
+      const fullResponse = await Promise.all(APIData);
+      const jsonList = await Promise.all(
+        fullResponse.map((value) => value.json())
+      );
+      const formed = jsonList.map((value: unknown) => {
+        return {
+          name: value.label,
+          data: value.data.map((v) => v.value),
+        };
+      });
+
+      return formed;
+    };
+    void converter().then((v) => {
+      setGraph(v);
+    });
+  }, [props.showDataId]);
 
   return (
     <>
